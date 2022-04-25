10d5dd83d379d6d32f06dbc4cfc3f3ee3acc247c
ankita21gaikwad <96906679+ankita21gaikwad@users.noreply.github.com>
2022-04-25 22:03:47 +0530

    Add test hooks

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,6 +6,20 @@ test('basic test', async ({ page }) => {
   await expect(title).toHaveText('Playwright');
 });
 
+
+test.describe('feature foo', () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto('https://playwright.dev/');
+  });
+
+  test('my test', async ({ page }) => {
+    // Assertions use the expect API.
+    await expect(page).toHaveURL('https://playwright.dev/');
+  });
+});
+
+
 // test.beforeEach(async ({ page }) => {
 //   await page.goto('https://demo.playwright.dev/todomvc');
 // });
